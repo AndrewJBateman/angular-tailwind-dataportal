7b16844fedb8e981005100d6888801d8c9369f12
AndrewJBateman <gomezbateman@yahoo.com>
2022-12-03 17:12:28 +0100

    refactor: replace db data for frontend grid

--- src/app/models/pharma.data.ts
+++ src/app/models/pharma.data.ts
@@ -1,7 +1,11 @@
 export interface PharmaData {
-  id: string,
-  title: string,
-  url: string,
-  source: string,
-  date: string,
+  drug_code: number;
+  class_name: string;
+  drug_identification_number: string;
+  brand_name: string;
+  descriptor: string;
+  number_of_ais: string;
+  ai_group_no: string;
+  company_name: string;
+  last_update_date: string;
 }
