1a5a40540abf02d9a21c8275f2942d115700c918
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-24 22:20:26 +0100

    refactor: use rxjs share(), null if no data, throwError not of

--- src/app/data-mngt/data.service.ts
+++ src/app/data-mngt/data.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, Signal } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, map, of, shareReplay } from 'rxjs';
+import { Observable, catchError, map, throwError, share } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { IPharmaData } from './pharma.data';
 import { State, StatusNotification } from './state.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -23,18 +23,17 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   loadData(): Observable<State<Array<IPharmaData>> | State<null>> {
-    const API_URL = environment.API_URL;
-    return this.http.get<Array<IPharmaData>>(`${API_URL}/api`).pipe(
-      shareReplay(1),
+    return this.http.get<Array<IPharmaData>>(`${environment.API_URL}/api`).pipe(
+      share(),
       map((data) => {
         return new State<Array<IPharmaData>>(
           StatusNotification.OK,
           data,
-          undefined
+          null
         );
       }),
       catchError((error: HttpErrorResponse) => {
-        return of(
+        return throwError(
           new State<Array<IPharmaData>>(
             StatusNotification.ERROR,
             undefined,
